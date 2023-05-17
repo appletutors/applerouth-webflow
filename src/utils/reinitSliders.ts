7f9d5ab6c97a3b5a8f95bae1d769cff1c04569c3
Paras Shah <parasshah195@gmail.com>
2023-05-17 19:31:01 +0530

    Redraw Webflow sliders after updating events
    Fixes slider functionality not working post event data update

--- src/utils/reinitSliders.ts
+++ src/utils/reinitSliders.ts
@@ -1,6 +1,5 @@
 export const reInitSliders = () => {
-  // TODO: `redraw` function not fully re-initializing the sliders
-  // window.Webflow.require('slider').redraw();
+  window.Webflow?.require('slider').redraw();
 
   // resize event triggering the slider refresh
   window.dispatchEvent(new Event('resize'));
